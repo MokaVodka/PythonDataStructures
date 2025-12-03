2f62557c10e818782b312db92eeb01645f0b4bf4
MokaVodka <hoangnhito@gmail.com>
2025-12-03 02:38:03 +0100

    Deque test

---
+++ Assignment2/Lecture5/tests/test_deque.py
@@ -0,0 +1,90 @@
+import pytest
+from util_test import create_Deque
+
+
+# Assert
+def assert_result(result, expect):
+    assert result == expect, f"Exp {expect}, got {result}"
+
+
+# Test add and get
+def test_add_first():
+    deque = create_Deque()
+
+    deque.add_first('Alpha')
+    deque.add_first('Beta')
+    deque.add_first('Gamma')
+
+    assert_result(deque.size, 3)
+    assert_result(deque.get_first(), 'Gamma')
+    assert_result(deque.get_last(), 'Alpha')
+
+
+def test_add_last():
+    deque = create_Deque()
+
+    deque.add_last('Alpha')
+    deque.add_last('Beta')
+    deque.add_last('Gamma')
+
+    assert_result(deque.size, 3)
+    assert_result(deque.get_first(), 'Alpha')
+    assert_result(deque.get_last(), 'Gamma')
+
+
+# Test remove
+def test_remove_first():
+    deque = create_Deque()
+    deque.add_last('Alpha')
+    deque.add_last('Beta')
+    deque.add_last('Gamma')
+
+    removed = deque.remove_first()
+    assert_result(removed, 'Alpha')
+    assert_result(deque.get_first(), 'Beta')
+    assert_result(deque.get_last(), 'Gamma')
+
+
+def test_remove_last():
+    deque = create_Deque()
+    deque.add_last('Alpha')
+    deque.add_last('Beta')
+    deque.add_last('Gamma')
+
+    removed = deque.remove_last()
+    assert_result(removed, 'Gamma')
+    assert_result(deque.get_first(), 'Alpha')
+    assert_result(deque.get_last(), 'Beta')
+
+
+# Test empty
+def test_empty_deque():
+    deque = create_Deque()
+    assert_result(deque.size, 0)
+
+    # Access node
+    genericMessage = 'Queue is empty, nothing to '
+    expect = genericMessage + 'access'
+
+    with pytest.raises(IndexError) as exc_info:
+        deque.get_first()
+    result = str(exc_info.value)
+    assert_result(result, expect)
+
+    with pytest.raises(IndexError) as exc_info:
+        deque.get_last()
+    result = str(exc_info.value)
+    assert_result(result, expect)
+
+    # Remove node
+    expect = genericMessage + 'remove'
+
+    with pytest.raises(IndexError) as exc_info:
+        deque.remove_first()
+    result = str(exc_info.value)
+    assert_result(result, expect)
+
+    with pytest.raises(IndexError) as exc_info:
+        deque.remove_last()
+    result = str(exc_info.value)
+    assert_result(result, expect)
