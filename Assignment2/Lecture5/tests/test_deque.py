838f2860e8a895ae9058a42712eee8b82b1c3ac8
MokaVodka <hoangnhito@gmail.com>
2025-12-04 14:46:55 +0100

    Added pytest.fixtures

--- Assignment2/Lecture5/tests/test_deque.py
+++ Assignment2/Lecture5/tests/test_deque.py
@@ -7,50 +7,50 @@ def assert_result(result, expect):
     assert result == expect, f"Exp {expect}, got {result}"
 
 
-# Test add and get
-def test_add_first():
+@pytest.fixture
+def deque_add_first():
     deque = create_Deque()
-
     deque.add_first('Alpha')
     deque.add_first('Beta')
     deque.add_first('Gamma')
+    return deque
 
+
+@pytest.fixture
+def deque_add_last():
+    deque = create_Deque()
+    deque.add_last('Alpha')
+    deque.add_last('Beta')
+    deque.add_last('Gamma')
+    return deque
+
+
+# Test add and get
+def test_add_first(deque_add_first):
+    deque = deque_add_first
     assert_result(deque.size, 3)
     assert_result(deque.get_first(), 'Gamma')
     assert_result(deque.get_last(), 'Alpha')
 
 
-def test_add_last():
-    deque = create_Deque()
-
-    deque.add_last('Alpha')
-    deque.add_last('Beta')
-    deque.add_last('Gamma')
-
+def test_add_last(deque_add_last):
+    deque = deque_add_last
     assert_result(deque.size, 3)
     assert_result(deque.get_first(), 'Alpha')
     assert_result(deque.get_last(), 'Gamma')
 
 
 # Test remove
-def test_remove_first():
-    deque = create_Deque()
-    deque.add_last('Alpha')
-    deque.add_last('Beta')
-    deque.add_last('Gamma')
-
+def test_remove_first(deque_add_last):
+    deque = deque_add_last
     removed = deque.remove_first()
     assert_result(removed, 'Alpha')
     assert_result(deque.get_first(), 'Beta')
     assert_result(deque.get_last(), 'Gamma')
 
 
-def test_remove_last():
-    deque = create_Deque()
-    deque.add_last('Alpha')
-    deque.add_last('Beta')
-    deque.add_last('Gamma')
-
+def test_remove_last(deque_add_last):
+    deque = deque_add_last
     removed = deque.remove_last()
     assert_result(removed, 'Gamma')
     assert_result(deque.get_first(), 'Alpha')
