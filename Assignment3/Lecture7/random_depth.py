d6e1f017a0a5306e4a3743b6e1ed3889c3e9bd93
MokaVodka <hoangnhito@gmail.com>
2025-12-16 23:40:18 +0100

    random_depth.py

--- Assignment3/Lecture7/random_depth.py
+++ Assignment3/Lecture7/random_depth.py
@@ -1,27 +1,22 @@
-import matplotlib
+import matplotlib.pyplot as plot
 import random
+import math
 import BstSet as BST
-
-
-def generate_data(h):
-    # Trees has 2h − 1 unique (non-duplicate) elements
-    elements = (2 * h) - 1
-    data = set()
-    while len(data) < elements:
-        data.add(random.randint(0, 100))
-    return data
 
 
 # Computes the mean max depth of a BST
 def mean_max_depth(h):
+    # Trees has 2h − 1 unique (non-duplicate) elements
+    elements = (2 * h) - 1
     maxDepths = []
 
     for _ in range(0, 10):
         bst = BST.BstSet()
-        data = generate_data(h)
 
-        for n in data:
-            bst.add(n)
+        while bst.size() < elements:
+            data = random.randint(0, 100)
+            bst.add(data)
+
         maxDepths.append(bst.max_depth())
 
     return sum(maxDepths)/len(maxDepths)
@@ -29,41 +24,61 @@ def mean_max_depth(h):
 
 def generate_dataset_for_plot():
     # h = 5, 6, ..., 19, 20
-    treeSizes = [(2 * h) - 1 for h in range(5, 21)]
-    depths = [h for h in range(5, 21)]
+    treeDepths = [h for h in range(5, 21)]
+    treeSizes = [(2 * h) - 1 for h in treeDepths]
     meanDepths = []
 
     # Calculate mean over 10 runs
-    for h in depths:
+    for h in treeDepths:
         meanDepths.append(mean_max_depth(h))
 
-    return treeSizes, depths, meanDepths
+    return treeSizes, treeDepths, meanDepths
 
 
-def plot_treeSizes_meanDepths():
+def log2_treeSizes(treeSizes):
+    logTreeSizes = [math.log(size, 2) for size in treeSizes]
+    return logTreeSizes
+
+
+def plot_diagram(treeSizes, treeDepths, meanDepths, isLog):
+    _, plots = plot.subplots(1, 2, figsize=(13, 5))
+    meanDepthPlot, treeDepthPlot = plots
+
+    # Configure plot (visuals)
+    markers = ['+', 'x']
+    colors = ['r', 'b']
 
     # Plot tree sizes vs Average of Max Depth
+    meanDepthPlot.scatter(treeSizes, meanDepths,
+                          marker=markers[0], c=colors[0])
+    meanDepthPlot.plot(treeSizes, meanDepths, c=colors[0], alpha=0.3)
 
     # Plot tree sizes vs Depth of Complete Tree (i.e 5, 6, ..., 19, 20)
+    treeDepthPlot.scatter(treeSizes, treeDepths,
+                          marker=markers[1], c=colors[1])
+    treeDepthPlot.plot(treeSizes, treeDepths, c=colors[1], alpha=0.3)
 
-    pass
+    # Configure plot (general)
+    label = 'Log base 2 tree sizes' if isLog else 'Tree sizes'
 
+    meanDepthPlot.set_title(f'{label} vs Mean max depth')
+    meanDepthPlot.set_xlabel(f'{label}')
+    meanDepthPlot.set_ylabel('Mean max depth of 10 runs with random lists')
 
-def plot():
-    # Plot log2 tree sizes vs Average of Max Depth
+    treeDepthPlot.set_title(f'{label} vs Tree depth')
+    treeDepthPlot.set_xlabel(f'{label}')
+    treeDepthPlot.set_ylabel('Tree depths')
 
-    # Plot log2 tree sizes vs Depth of Complete Tree (i.e 5, 6, ..., 19, 20)
-
-    # Is it roughly a straight line?
-    # We expect the two diagrams (in a single 2 × 1 pattern) to pop up.
-
-    # expect a O(log(n)) behavior
-    pass
+    plot.tight_layout()
+    plot.show()
 
 
 def experiment():
-    treeSizes, depths, meanDepths = generate_dataset_for_plot()
-    plot_treeSizes_meanDepths()
+    treeSizes, treeDepths, meanDepths = generate_dataset_for_plot()
+    plot_diagram(treeSizes, treeDepths, meanDepths, False)
+
+    treeSizes = log2_treeSizes(treeSizes)
+    plot_diagram(treeSizes, treeDepths, meanDepths, True)
 
 
 experiment()
