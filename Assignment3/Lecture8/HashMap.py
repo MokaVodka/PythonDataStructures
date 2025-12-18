7b5285efbca7e9eca408a86db3948793493c1345
MokaVodka <hoangnhito@gmail.com>
2025-12-18 12:08:43 +0100

    Finding prime numbers

--- Assignment3/Lecture8/HashMap.py
+++ Assignment3/Lecture8/HashMap.py
@@ -11,24 +11,66 @@ class HashMap:
         self.capacity = self.next_prime(init_capacity)
         self.table = [None] * self.capacity
 
+    # Check if number is prime
+    def is_prime(self, n):
+        # Base case: 0 and 1 are not prime
+        if n == 1 or n == 0:
+            return False
+
+        # Simple case: 2 and 3 are prime
+        if n == 2 or n == 3:
+            return True
+
+        # Mults of 2 and 3 are not prime
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+
+        # Pass all cases, check if number is prime
+        # Up until square root of n (for n = sqrt(n)^2)
+        # Increment by 6 to skip mults of 2 and 3
+        for i in range(5, math.sqrt(n) + 1, 6):
+            if n % i == 0:
+                return False
+
+            # Because we increment by 6
+            # Check for mod for non-(mult 2 or mult 3) numbers
+            # 5 -6- 7 -8- -9- -10- | 11 -12- 13 -14- 15- -16- | ... (i + 2)
+            if n % (i + 2) == 0:
+                return False
+
+        # Pass loop check, number is prime
+        return True
+
     # Return next larger prime number
     def next_prime(self, n):
-        pass
+        # Base case: input is 1
+        if n <= 1:
+            return 2
+
+        # Start to find prime, set initial start point n
+        prime = n
+        found = False
+
+        while not found:
+            prime = prime + 1
+            found = self.is_prime(prime)
+
+        return prime
 
     # Display table content. Only non-empty slots
     def __str__(self):
         pass
 
     # Element count
-    def get_size(self): 
+    def get_size(self):
         pass
 
     # Table size
-    def get_capacity(self): 
+    def get_capacity(self):
         pass
 
     # Load factor
-    def get_loadfactor(self): 
+    def get_loadfactor(self):
         pass
 
     # Prime number based hash function for strings
